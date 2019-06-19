ed2ab68c2cc80a6626519cafce8ca8e497ce0c6a
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 21:47:54 +0800

    Add files via upload

--- Script-013-FacetGrid.py
+++ Script-013-FacetGrid.py
@@ -1,39 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
-#
-# libraries
-import numpy as np
-import seaborn as sb
-import pandas as pd
-import matplotlib.pyplot as plt
 import os
-#
-# import data
-os.chdir('/Users/pauline/Documents/Python')
-dfM = pd.read_csv("Tab-Bathy.csv")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
 sb.set_style('darkgrid')
+sb.set_context('paper')
+
+os.chdir('/Users/pauline/Documents/Python')
+
+# defining a loop
+profiles_nrs = list(map(lambda x: x, range(25)))
+profiles_list = []
+for i in profiles_nrs:
+    profiles_list.append('profile{}'.format(i + 1))
+
+# generating data frame
+dfM = pd.read_csv("Tab-Bathy.csv")
 df = dfM.melt(id_vars=['observ'], 
-              value_vars=["profile1", "profile2", "profile3", "profile4", "profile5", 
-                          "profile6", "profile7","profile8", "profile9", "profile10",
-                          "profile11", "profile12", "profile13", "profile14", "profile15",
-                          "profile16", "profile17", "profile18", "profile19", "profile20",
-                          "profile21", "profile22", "profile23", "profile24", "profile25"],
-              var_name='Profiles', value_name='Depths')
-#
-# Create a grid : initialize it
-g = sb.FacetGrid(df, col='Profiles', hue='Profiles', col_wrap=5, )
-#
-# Add the line over the area with the plot function
-g = g.map(plt.plot, 'observ', 'Depths')
-#
-# Fill the area with fill_between
-g = g.map(plt.fill_between, 'observ', 'Depths', alpha=0.1).set_titles("{col_name} Profile")
-#
-# Control the title of each facet
-g = g.set_titles("{col_name}")
-#
-# Add a title for the whole plot
-plt.subplots_adjust(top=0.92)
-g = g.fig.suptitle('Bathymetric cross-section 25 profiles of the Mariana Trench: \n518 observation points in each profile', fontsize=14, fontfamily='serif')
+              value_vars=profiles_list,
+              var_name='Profiles', value_name='Depths'
+              )
+
+# initializing grid
+g = sb.FacetGrid(df, col='Profiles', hue='Profiles', col_wrap=5)
+g.fig.suptitle('Mariana Trench: 25 cross-section profiles with \n518 samples in each',
+               fontsize=14, fontfamily='serif'
+               )
+# add line
+g.map(plt.plot, 'observ', 'Depths')
+# fill area
+g.map(plt.fill_between, 'observ', 'Depths', alpha=0.1
+          ).set_titles("{col_name} Profile")
+g.set_titles("{col_name}")
+
+
+g.fig.suptitle('Bathymetric cross-section 25 profiles of the Mariana Trench: \n518 observation points in each profile',
+               fontsize=14, fontfamily='serif'
+               )
+
+# visualizing
+plt.tight_layout()
 plt.subplots_adjust(bottom=0.05,top=0.90)
+plt.savefig('plot_FacetGrid.png', dpi=300)
 plt.show()
